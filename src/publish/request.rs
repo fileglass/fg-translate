0e546f544fbb47f4f4c249f9b6598f46d2b4f65f
Léderer András <andrew@file.glass>
2021-10-01 17:32:57 +0200

    fix(requests): fix UA error

--- src/publish/request.rs
+++ src/publish/request.rs
@@ -1,7 +1,12 @@
 use reqwest;
 
 pub fn uploadFile(apiKey: &str, filePath: &str) -> Result<String, reqwest::Error> {
-    let client = reqwest::blocking::Client::new();
+    static APP_USER_AGENT: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+    );
+    let client = reqwest::blocking::Client::builder().user_agent(APP_USER_AGENT).build().unwrap();
     let form = reqwest::blocking::multipart::Form::new().file("file", filePath).unwrap();
     let res = client.post(format!("https://api.file.glass/v3/common/translation/upload?access_key={}", apiKey))
         .multipart(form)
--- src/utils/shared.rs
+++ src/utils/shared.rs
@@ -7,7 +7,13 @@ pub fn readCli(mut line: &mut String) {
 
 
 pub fn fetchLocale(key: &str) -> Result<String, reqwest::Error> {
-    let text = reqwest::blocking::get(format!("https://api.file.glass/v3/common/translation?access_key={}", key))?.text()?;
+    static APP_USER_AGENT: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+    );
+    let client = reqwest::blocking::Client::builder().user_agent(APP_USER_AGENT).build().unwrap();
+    let text = client.get(format!("https://api.file.glass/v3/common/translation?access_key={}", key)).send().unwrap().text().unwrap();
     Ok(text)
 }
 
